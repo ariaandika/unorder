66e48d56c392011e7405387074e59ed655d86b0a
ariaandika <andikaakun17@gmail.com>
2024-10-30 15:29:54 +0700

    change htmx script path

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -3,20 +3,21 @@ use std::io;
 use tokio::net::TcpListener;
 use tower_http::services::ServeFile;
 
-use unorder::orders;
+use unorder::{orders, auth};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let tcp = TcpListener::bind("localhost:3000").await?;
 
     let route = Router::new()
+        .route("/login", get(auth::Views::login))
         .nest(
             "/orders",
             Router::new()
                 .route("/", get(orders::Views::show))
                 .route("/new", get(orders::Views::new)),
         )
-        .route_service("/hx.js", ServeFile::new("static/htmx.js"));
+        .route_service("/hx.js", ServeFile::new("target/hx.js"));
 
     axum::serve(tcp, route).await
 }
